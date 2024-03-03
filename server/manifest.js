8e229e2a9479f52fce0ef9a90bca08bae2654a69
Borger Mathis <mathis.borger@etu.unilim.fr>
2024-03-03 13:33:00 +0100

    initial commit with a welcome email and a film library

--- server/manifest.js
+++ server/manifest.js
@@ -6,7 +6,7 @@ const Toys = require('@hapipal/toys');
 const Schwifty = require('@hapipal/schwifty');
 
 // Pull .env into process.env
-Dotenv.config({ path: `${__dirname}/.env` });
+Dotenv.config({ path: `${__dirname}/../.env` });
 
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
@@ -15,7 +15,7 @@ module.exports = new Confidence.Store({
         port: {
             $param: 'PORT',
             $coerce: 'number',
-            $default: 3000
+            $default: 3307
         },
         debug: {
             $filter: 'NODE_ENV',
@@ -35,7 +35,7 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-                plugin: './plugins/swagger',
+                plugin: './plugins/swagger'
             },
             {
                 plugin: '@hapipal/schwifty',
@@ -45,13 +45,13 @@ module.exports = new Confidence.Store({
                     $base: {
                         migrateOnStart: true,
                         knex: {
-                            client: 'sqlite3',
-                            useNullAsDefault: true,     // Suggested for sqlite3
+                            client: 'mysql2',
                             connection: {
-                                filename: ':memory:'
-                            },
-                            migrations: {
-                                stub: Schwifty.migrationsStubPath
+                                host: process.env.DB_HOST || '127.0.0.1',
+                                port: process.env.DB_PORT || '3306',
+                                user: process.env.DB_USER || 'root',
+                                password: process.env.DB_PASSWORD || 'hapi',
+                                database: process.env.DB_DATABASE || 'user'
                             }
                         }
                     },
